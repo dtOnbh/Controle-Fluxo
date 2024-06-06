f530e30ef536c800860fface6319e376a53986e3
dtOnbh <daltonsilva.felix@gmail.com>
2024-06-05 21:15:42 -0300

    IF ou Else

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
      public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 30.0;
  
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
               saldo = saldo - valorSolicitado;
+              System.out.println("Seu Saldo é: " + saldo +" reais");
  
-         System.out.println("Seu Saldo é: " + saldo +" reais");
+     
+          }
+        else
+            System.out.println("Saldo Insuficiente");
      }   
 }
